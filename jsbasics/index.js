91eec528b8402cd570573b065a2098a450cc8cc2
Parbat Karki <karkiparbat9@gmail.com>
2025-12-10 09:45:06 +0545

    initial js basics

---
+++ jsbasics/index.js
@@ -0,0 +1,17 @@
+// let person = {
+//     name:"Parbat",
+//     age:20,
+//     class: "XII"
+// }
+
+// console.log(person.name);
+
+// let nameOfAnimal = ["dog","cat","rabbit"]
+// console.log(nameOfAnimal[0])
+
+// let a =50;
+// console.log("I am " +a)
+
+//template literals
+let name = "ram";
+console.log(`my name is ${name}`)
