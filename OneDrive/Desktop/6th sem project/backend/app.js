bba87767f3ba29297065e51bf687e9c48a1481e8
Parbat Karki <karkiparbat9@gmail.com>
2025-12-29 09:51:18 +0545

    axios installed and both running

--- OneDrive/Desktop/6th sem project/backend/app.js
+++ OneDrive/Desktop/6th sem project/backend/app.js
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import authRouter from './src/routes/auth.js'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth', authRouter)
 
